69c09a94134c893fa081be6a2dbe67914ee93bd1
James Oshomah <Jamiebones2000@yahoo.co.uk>
2022-04-01 08:49:20 +0100

    fix publication of books

--- imports/api/books/server/publications.js
+++ imports/api/books/server/publications.js
@@ -1,8 +1,10 @@
-import { Meteor } from 'meteor/meteor';
-import BookCollection from '../books';
+import { Meteor } from "meteor/meteor";
+import BookCollection from "../books";
 
-
-Meteor.publish('books.allBooks', function allBooks () {
-    //return all books in the collection
-    return BookCollection.find();
+Meteor.publish("books.allBooks", function allBooks() {
+  //return all books in the collection where the coverImage and bookurl exist and not null
+  return BookCollection.find({
+    coverImage: { $exists: true, $ne: null },
+    bookurl: { $exists: true, $ne: null },
+  });
 });
